4e8fc53b7ea35ae0170b6bf5143c3b972cee9bfa
Harry <hdvo1@uci.edu>
2019-02-08 22:51:15 -0800

    NetworkUltis /w Test

--- app/src/main/java/com/example/andyfetodia/mjapp/MainActivity.java
+++ app/src/main/java/com/example/andyfetodia/mjapp/MainActivity.java
@@ -1,16 +1,38 @@
 package com.example.andyfetodia.mjapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getName();
 
+    private static final String MJ_REQUEST_URL = "https://itunes.apple.com/search?term=Michael+jackson";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new AsyncTask<Void, Void, List<Song>>() {
+
+            @Override
+            protected void onPostExecute(List<Song> songs) {
+                for(int i =0; i< songs.size();i++){
+                    Log.e(LOG_TAG,songs.get(i).getTrackName());
+                }
+
+            }
+
+            @Override
+            protected List<Song>  doInBackground(Void... voids) {
+                List<Song> mjSong = NetworkUtils.fetchEarthquakeData(MJ_REQUEST_URL);
+                return mjSong;
+            }
+        }.execute();
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/andyfetodia/mjapp/NetworkUtils.java
@@ -0,0 +1,204 @@
+package com.example.andyfetodia.mjapp;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkUtils {
+    private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
+
+
+    /**
+     *
+     * @param requestUrl
+     * @return
+     */
+    public static List<Song> fetchEarthquakeData(String requestUrl) {
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+        }
+
+        // Extract relevant fields from the JSON response and create a list of {@link Earthquake}s
+        List<Song> songs = extractFeatureFromJson(jsonResponse);
+
+        // Return the list of {@link Earthquake}s
+        return songs;
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
+    /**
+     * Fetch the json file to the app
+     * @param url
+     * @return
+     * @throws IOException
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // Closing the input stream could throw an IOException, which is why
+                // the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    /**
+     *
+     * @param songJSON
+     * @return
+     */
+    private static List<Song> extractFeatureFromJson(String songJSON) {
+        // If the JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(songJSON)) {
+            return null;
+        }
+
+        // Create an empty ArrayList that we can start adding earthquakes to
+        List<Song> songs = new ArrayList<>();
+
+        // Try to parse the JSON response string. If there's a problem with the way the JSON
+        // is formatted, a JSONException exception object will be thrown.
+        // Catch the exception so the app doesn't crash, and print the error message to the logs.
+        try {
+
+            // Create a JSONObject from the JSON response string
+            JSONObject baseJsonResponse = new JSONObject(songJSON);
+
+            // Extract the JSONArray associated with the key called "features",
+            // which represents a list of features (or earthquakes).
+            int size = baseJsonResponse.getInt("resultCount");
+            JSONArray songArray = baseJsonResponse.getJSONArray("results");
+
+            // For each earthquake in the earthquakeArray, create an {@link Earthquake} object
+            //for (int i = 0; i < earthquakeArray.length(); i++) {
+            for (int i = 0; i < size; i++) {
+                // Get a single earthquake at position i within the list of earthquakes
+                JSONObject currentSong = songArray.getJSONObject(i);
+
+/*                // For a given earthquake, extract the JSONObject associated with the
+                // key called "properties", which represents a list of all properties
+                // for that earthquake.
+                //JSONObject properties = currentEarthquake.getJSONObject("properties");
+
+                // Extract the value for the key called "mag"
+                double magnitude = properties.getDouble("mag");
+
+                // Extract the value for the key called "place"
+                String location = properties.getString("place");
+
+                // Extract the value for the key called "time"
+                long time = properties.getLong("time");
+
+                // Extract the value for the key called "url"
+                String url = properties.getString("url");
+
+                // Create a new {@link Earthquake} object with the magnitude, location, time,
+                // and url from the JSON response.
+                Earthquake earthquake = new Earthquake(magnitude, location, time, url);
+
+
+                // Add the new {@link Earthquake} to the list of earthquakes.
+                earthquakes.add(earthquake);*/
+
+                String artworkUrl100 = currentSong.getString("artworkUrl100");
+                String artistName = currentSong.getString("artistName");
+                String collectionName = currentSong.getString("collectionName");
+                String trackName = currentSong.getString("trackName");
+
+                Song newSong = new Song(artworkUrl100,artistName,collectionName,trackName);
+                songs.add(newSong);
+
+            }
+
+        } catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("QueryUtils", "Problem parsing the earthquake JSON results", e);
+        }
+
+        // Return the list of earthquakes
+        return songs;
+    }
+}
